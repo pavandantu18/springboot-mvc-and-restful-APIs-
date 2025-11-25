11d9c7b596c0d6b2fa6b10cbe56662d556706b41
pavandantu18 <pavandantu18@gmail.com>
2025-11-25 15:21:23 -0500

    init

---
+++ src/main/java/com/example/mvcArchitecture/MvcArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvcArchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcArchitectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mvcArchitecture/MvcArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvcArchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
