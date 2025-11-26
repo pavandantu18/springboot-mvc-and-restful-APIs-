87072911850cfeb6eda27739021079685a9b32a1
pavandantu18 <pavandantu18@gmail.com>
2025-11-25 21:26:14 -0500

    persistance layer and jpa repository

--- src/main/java/com/example/mvcArchitecture/controllers/EmployeeController.java
+++ src/main/java/com/example/mvcArchitecture/controllers/EmployeeController.java
@@ -1,6 +1,8 @@
 package com.example.mvcArchitecture.controllers;
 
 import com.example.mvcArchitecture.dto.EmployeeDTO;
+import com.example.mvcArchitecture.entity.EmployeeEntity;
+import com.example.mvcArchitecture.repositories.EmployeeRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -9,31 +11,28 @@ import java.util.List;
 @RestController
 @RequestMapping(path = "/employees")
 public class EmployeeController {
-    @GetMapping(path = "/getSecretMessage")
-    public String getMySuperSecretMessage() {
-        return "Secret Message: asdfrhufh@rij";
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
     }
 
-//    @GetMapping(path = "/{employeeId}")
-//    public EmployeeDTO getEmployeeById(@PathVariable Long employeeId) {
-//        return new EmployeeDTO(employeeId,"Pavan", "pavan@gmail.com",23, LocalDate.of(2024,1,2), true);
-//    }
     @GetMapping(path = "/{employeeId}")
-    public EmployeeDTO getEmployeeById(@PathVariable(name = "employeeId") Long id) {
-        return new EmployeeDTO(id,"Pavan", "pavan@gmail.com",23, LocalDate.of(2024,1,2), true);
+    public EmployeeEntity getEmployeeById(@PathVariable(name = "employeeId") Long id) {
+        return employeeRepository.findById(id).orElse(null);
     }
 
 //    http://localhost:8080/employees?age=12&sortBy=age
     @GetMapping
-    public String getAllEmployees(@RequestParam(required = false) Integer age,
-                                  @RequestParam(required = false) String sortBy) {
-        return "Hi age" + age + " " + sortBy;
+    public List<EmployeeEntity> getAllEmployees(@RequestParam(required = false) Integer age,
+                                                @RequestParam(required = false) String sortBy) {
+        return employeeRepository.findAll();
     }
 
     @PostMapping
-    public EmployeeDTO createEmployee(@RequestBody EmployeeDTO inputEmployee) {
-        inputEmployee.setId(100L);
-        return inputEmployee;
+    public EmployeeEntity createEmployee(@RequestBody EmployeeEntity inputEmployee) {
+        return employeeRepository.save(inputEmployee);
     }
 
     @PutMapping
